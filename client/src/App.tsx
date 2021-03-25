1fed76accb1f1a0eed37c7ee53302b14f315a403
gabrielnascr <gr153tt@gmail.com>
2021-03-25 16:05:15 -0300

    add global context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { AuthProvider } from './context/auth';
+import ContextProvider from './context';
 import Routes from './routes';
 import history from './services/history';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <AuthProvider>
+    <ContextProvider>
       <Router history={history}>
         <Routes />
         <GlobalStyle />
       </Router>
-    </AuthProvider>
+    </ContextProvider>
   );
 }
 
---
+++ client/src/context/index.tsx
@@ -0,0 +1,16 @@
+import { AuthProvider } from './auth';
+
+interface Props {
+    children?: React.ReactNode
+}
+
+function ContextProvider({ children }: Props) {
+    return (
+        <AuthProvider>
+            {children}
+        </AuthProvider>
+
+    )
+}
+
+export default ContextProvider;
